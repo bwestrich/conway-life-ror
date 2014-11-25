a17117f63087e7cac76def1266043597a214ffe7
Brian Westrich <bw@mcwest.com>
2014-11-24 20:12:57 -0600

    remove unused objects

--- db/schema.rb
+++ db/schema.rb
@@ -9,40 +9,40 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121231194410) do
+ActiveRecord::Schema.define(version: 20121231194410) do
 
-  create_table "blog_posts", :force => true do |t|
+  create_table "blog_posts", force: true do |t|
     t.string   "name"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "board2s", :force => true do |t|
+  create_table "board2s", force: true do |t|
     t.string   "nrows"
     t.string   "ncolumns"
     t.string   "percentLive"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "board3s", :force => true do |t|
+  create_table "board3s", force: true do |t|
     t.string   "nrows"
     t.string   "ncolumns"
     t.string   "percentLive"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "posts", :force => true do |t|
+  create_table "posts", force: true do |t|
     t.string   "name"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
